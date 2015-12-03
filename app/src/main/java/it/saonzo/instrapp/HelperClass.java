00547600889076bc5d7b0b2ffd4d4a298e677522
SCPTeam <yugo.amaril@gmail.com>
2015-12-03 16:35:52 +0100

    HelperClass improved

--- app/src/main/java/it/saonzo/instrapp/HelperClass.java
+++ app/src/main/java/it/saonzo/instrapp/HelperClass.java
@@ -2,24 +2,24 @@ package it.saonzo.instrapp;
 
 
 import android.content.Context;
-import android.net.http.AndroidHttpClient;
+//import android.net.http.AndroidHttpClient;
 import android.os.Environment;
 import android.util.Log;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.auth.Credentials;
-import org.apache.http.client.CookieStore;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.impl.client.ClientParamsStack;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.Header;
-import org.apache.http.protocol.HttpContext;
+//import org.apache.http.HttpResponse;
+//import org.apache.http.auth.Credentials;
+//import org.apache.http.client.CookieStore;
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.client.methods.HttpGet;
+//import org.apache.http.client.methods.HttpPost;
+//import org.apache.http.client.methods.HttpUriRequest;
+//import org.apache.http.cookie.Cookie;
+//import org.apache.http.impl.client.ClientParamsStack;
+//import org.apache.http.impl.client.DefaultHttpClient;
+//import org.apache.http.params.BasicHttpParams;
+//import org.apache.http.params.HttpParams;
+//import org.apache.http.Header;
+//import org.apache.http.protocol.HttpContext;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -34,16 +34,17 @@ import it.saonzo.annotations.ClassWithInstrMethods;
 public class HelperClass {
     public static final String TAG = "###-INSTR-###";
     private static int var = 0;
-    public static HashMap<Integer, String> env = new HashMap<Integer,String>();
+    public static HashMap<Object, String> env = new HashMap<Object,String>();
+    private static FileWriter fw = null;
 
     private static String freshVar() {
         return "t_" + var++;
     }
 
-    public static void CreateVariable(int hash, String type, String constructor) {
+    public static void CreateVariable(Object o, String type, String constructor) {
         String var = freshVar();
-        Log(type + " " + var + " = " + constructor + ";\n");
-        env.put(new Integer(hash), var);
+        Log(type + " " + var + " = " + constructor);
+        env.put(o, var);
     }
 
     public static String simpleDecl(String type) {
@@ -57,7 +58,14 @@ public class HelperClass {
     }
 
     public static void Log(String codeline) {
-        plog("\t" + codeline + "\n");
+        if(codeline.endsWith(";"))
+            plog("\t" + codeline + "\n");
+        else
+            plog("\t" + codeline + ";\n");
+    }
+
+    public static void LogComment(String codeline) {
+        plog("\t/* " + codeline + " */\n");
     }
 
     public static void LogException(String msg) {
@@ -66,112 +74,39 @@ public class HelperClass {
 
 
     public static String get(Object o) {
-        return env.get(new Integer(o.hashCode()));
+        if(env.containsKey(o))
+            return env.get(o);
+        else
+            return "/* could not find in environment */ \"" + o.toString() + "\"";
     }
 
     public static boolean containsKey(Object o) {
         return env.containsKey(o.hashCode());
     }
 
-    public static void LogHttpClient(HttpClient client) {
+    private static void plog(String log) {
 
-        String log = client.toString() + "\n";
-
-        CookieStore cs;
-        if(client instanceof DefaultHttpClient)
-            cs = ((DefaultHttpClient)client).getCookieStore();
-        else
-            cs = null;
-
-        if(cs != null) {
-            for(Cookie c : cs.getCookies()) {
-                log += "COOKIE > " + c.getName() + " = " + c.getValue() + "\n";
-            }
-        }
-
-        plog(log);
-    }
-
-    public static void LogHttpUriRequest(HttpUriRequest req) {
-
-        String log = req.toString() +":\n" +
-                "> " + req.getMethod() + " " + req.getURI().toString() + "\n";
-
-        for(Header h : req.getAllHeaders()) {
-            log += "HEADER > " + h.getName() + " = " + h.getValue() + "\n";
-        }
-
-        if(req instanceof  HttpPost) {
+        if(fw == null) {
 
             try {
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                ((HttpPost) req).getEntity().writeTo(baos);
-                baos.flush();
-                String body = new String(baos.toByteArray());
-                log += "BODY > " + body + "\n";
+                File saveFile = new File(Environment.getExternalStorageDirectory(), "instrlog.txt");
+                if (!saveFile.exists())
+                    saveFile.createNewFile();
+                fw = new FileWriter(saveFile, true);
+            } catch (IOException e) {
+                Log.e(TAG, e.toString());
             }
-            catch (Exception e) {}
+
         }
 
-        plog(log);
-    }
-
-    private static void plog(String log) {
-        FileWriter fw;
-
-        try {
-            File saveFile = new File(Environment.getExternalStorageDirectory(), "instrlog.txt");
-            if(!saveFile.exists())
-                saveFile.createNewFile();
-            fw = new FileWriter(saveFile, true);
-
-            fw.write(log);
-
-            fw.flush();
-            fw.close();
-        } catch (IOException e) {
-            Log.e(TAG, e.toString());
-        }
-        Log.d(TAG, log);
-    }
-
-    public static void LogCredentials(Credentials c) {
-        String log = "USERNAME > " + c.getUserPrincipal().toString() + "\n";
-        log += "PASSWORD > " + c.getPassword() + "\n";
-
-        plog(log);
-
-    }
-
-    public static void LogHttpRespose(HttpResponse res) {
-        String log = res.toString() +":\n" +
-                "> " + res.getStatusLine().getStatusCode() + " " + res.getStatusLine().getReasonPhrase() + "\n";
-
-        for(Header h : res.getAllHeaders()) {
-            log += "HEADER > " + h.getName() + " = " + h.getValue() +"\n";
-        }
-
-        try {
-            log += "CONTENT  > " + res.getEntity().getContentType().getName();
-            log += " = " + res.getEntity().getContentType().getValue();
-            log += "; " + res.getEntity().getContentEncoding().getName();
-            log += " = " + res.getEntity().getContentEncoding().getValue() + "\n";
-        }
-        catch (NullPointerException e) { log += "\n"; }
-        plog(log);
-    }
-
-/*    public static String map2string(Map<String,List<String>> m) {
-        String c = "";
-        for(String k : m.keySet()) {
-            c += k + " = ";
-            for(String t : m.get(k)) {
-                c += t + " ; ";
+        if(fw != null)
+            try {
+                fw.write(log);
+                fw.flush();
+            } catch (IOException e) {
+                Log.e(TAG, e.toString());
             }
-            c += "\n";
-        }
-        return c;
-    }*/
+    }
 
     public static String map2string(Map<String, ?> m) {
         String c = "";
